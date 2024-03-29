110aaaa4f059f0f2c7735b4f608a1fec35b5aed8
Fluid Senses Ltd <43846575+kcijordan23@users.noreply.github.com>
2024-03-29 15:02:05 +0000

    Created Headings up to 22:40 on 2021-05-16

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
 import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
@@ -17,6 +18,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${monsterrat.variable} font-mont bg-light w-full min-h-screen`}>
+        <NavBar/>
         <Component {...pageProps} />
       </main>
     </>
